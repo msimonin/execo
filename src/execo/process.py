msimonin/execo

commit 68a28df182140433a914d7c19091ea806fd13df1

[execo] process: py3: don't crash on decoding irregular bytes in process output

src/execo/process.py
@@ -33,7 +33,7 @@ import threading, time, pipes, sys
 
 if sys.version_info >= (3,):
     import codecs, locale
-    _decode = lambda s: codecs.decode(s, locale.getpreferredencoding())
+    _decode = lambda s: codecs.decode(s, encoding=locale.getpreferredencoding(), errors='surrogateescape')
 else:
     _decode = lambda s: s
 

commit 73680bc75089324b4fdb0f35613d5eca63360161

process: py3: support python 3.4 and below when decoding process output
The keyword syntax for codecs.decode() introduced in 68a28df182 is only
valid starting from Python 3.5.  For older versions of Python (3.0 to
3.4), it is necessary to pass arguments without keywords.  This is also
valid for 3.5 and later.

Fixes: 68a28df182 ("[execo] process: py3: don't crash on decoding irregular bytes in process output")

src/execo/process.py
@@ -33,7 +33,7 @@ import threading, time, pipes, sys
 
 if sys.version_info >= (3,):
     import codecs, locale
-    _decode = lambda s: codecs.decode(s, encoding=locale.getpreferredencoding(), errors='surrogateescape')
+    _decode = lambda s: codecs.decode(s, locale.getpreferredencoding(), 'surrogateescape')
 else:
     _decode = lambda s: s
 